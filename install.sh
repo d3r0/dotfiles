1b4db0d6235cb6bf50c2acb04a696a79c470a158
Marcel Arndt <marcel.arndt@hotmail.de>
2022-01-23 09:43:23 +0000

    add desktop notification configuration with dunst

--- install.sh
+++ install.sh
@@ -35,6 +35,11 @@ ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 #########
 # Fonts #
 #########
-
 mkdir -p "$XDG_DATA_HOME"
 cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+#########
+# Dunst #
+#########
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
