c0849ab5659af91ce97ab199071839cf3b2402d5
Marcel Arndt <marcel.arndt@hotmail.de>
2022-01-24 19:36:29 +0100

    add picom configuration

--- install.sh
+++ install.sh
@@ -7,6 +7,16 @@ mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
 ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 
+# install neovim plugin manager
+# [ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
+#   && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
+#   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+# ln -sf $DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+
+# install (or update) all the plugins
+nvim --noplugin +PlugUpdate +qa
+
 #######
 # X11 #
 #######
@@ -43,3 +53,15 @@ cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
 #########
 mkdir -p "$XDG_CONFIG_HOME/dunst"
 ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+
+########
+# tmux #
+########
+mkdir -p "$XDG_CONFIG_HOME/tmux"
+ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+
+#########
+# picom #
+#########
+mkdir -p "$XDG_CONFIG_HOME/picom"
+ln -sf "$DOTFILES/picom/picom.conf" "$XDG_CONFIG_HOME/picom/picom.conf"
